d5614d187293ef2486deb8bc42b946fc945be03d
ANSHUL GOLA <anshulgola5339@gmail.com>
2024-12-29 14:43:16 +0530

    Array For understanding
    This file is for beginners who just started learning JavaScript to get familiar with the syntax  that have been used in this file . there are more files to come !! keep learning

---
+++ arrray/index.js
@@ -0,0 +1,24 @@
+// array = a variable like structure that can hold more than one value 
+/*
+let fruits = ["apple", "orange" , "banana "];
+
+fruits[3] = "coconut";
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+*/
+// rest parameters = (...rest) allow a function work with a variable 
+//                  number of arguements by bundling them into an array
+
+/*function combineString(...strings){
+    return strings.join(" ");
+}
+
+
+const fullName = combineString("Mr.", "Spongebob" , "Squarepants", "III");
+
+console.log(fullName);
+
+*/
